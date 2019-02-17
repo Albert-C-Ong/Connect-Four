4827fdba285f794bf1c26e5a9eef70b43e88c5a6
Anferensis <anferensis@gmail.com>
2019-02-16 16:20:34 -0800

    Add files via upload

---
+++ ConnectFour.java
@@ -0,0 +1,22 @@
+
+/** ConnectFour.java
+ * 
+ * CS 151 Spring 2019
+ * Professor Katarzyna Tarnowska
+ * 
+ * The main program to the Connect Four game. 
+ * 
+ * @author Krish Ghiya, Holly Lind, and Albert Ong
+ * @since 16.02.2019
+ */
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ConnectFour {
+
+  public static void main(String[] args) {
+    
+    ConnectFourGameWindow test = new ConnectFourGameWindow();
+  }
+}
