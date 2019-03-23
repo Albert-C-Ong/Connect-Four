b3635e705fe020f450388b2b76743a0ac75675a3
Anferensis <anferensis@gmail.com>
2019-03-23 12:36:08 -0700

    Add files via upload

--- src/edu/sjsu/cs/cs151/connectfour/animationtest/MoveableShape.java
+++ src/edu/sjsu/cs/cs151/connectfour/animationtest/MoveableShape.java
@@ -1,6 +1,4 @@
-package edu.sjsu.cs.cs151.connectfour.animationtest;
 
-import java.awt.Graphics2D;
 
 /** MoveableShape.java
  * 
@@ -9,12 +7,18 @@ import java.awt.Graphics2D;
  * 
  * An interface for animated shapes.
  * 
- * @author Holly Lind
- * @since 3/18/2019
+ * @author Holly Lind and Albert Ong
+ * @since 23.03.2019
  */
+
+package edu.sjsu.cs.cs151.connectfour.animationtest;
+
+import java.awt.Graphics2D;
+
+
 public interface MoveableShape {
 
-	void draw(Graphics2D g2);
-	void move();
-	
+  void draw(Graphics2D g2);
+  void move();
+  
 }
--- src/edu/sjsu/cs/cs151/connectfour/animationtest/TileIcon.java
+++ src/edu/sjsu/cs/cs151/connectfour/animationtest/TileIcon.java
@@ -1,7 +1,3 @@
-package edu.sjsu.cs.cs151.connectfour.animationtest;
-
-import java.awt.*;
-import javax.swing.*;
 
 /** TileIcon.java
  * 
@@ -10,53 +6,58 @@ import javax.swing.*;
  * 
  * A class for a Tile's icon
  * 
- * @author Holly Lind
- * @since 3/18/2019
+ * @author Holly Lind and Albert Ong
+ * @since 23.03.2019
  */
+
+package edu.sjsu.cs.cs151.connectfour.animationtest;
+
+import java.awt.*;
+import javax.swing.*;
+
+
 public class TileIcon implements Icon {
 
-	/**
-	 * Creates a TileIcon
-	 * @param shape
-	 * @param width
-	 * @param height
-	 */
-	public TileIcon(MoveableShape shape, int width, int height) {
-		this.shape = shape;
-		this.width = width;
-		this.height = height;
-	}
-	
-	
-	/**
-	 * Access method for icon's width
-	 * @return width
-	 */
-	public int getIconWidth() {
-		return width;
-	}
-	
-	
-	/**
-	 * Access method for icon's height
-	 * @return height
-	 */
-	public int getIconHeight() {
-		return height;
-	}
-	
-	
-	/**
-	 * Draws icon
-	 */
-	public void paintIcon(Component c, Graphics g, int x, int y) {
-		Graphics2D g2 = (Graphics2D) g;
-		shape.draw(g2);
-	}
-	
-	
-	private int width;
-	private int height;
-	private MoveableShape shape;
-	
+  private int width;
+  private int height;
+  private MoveableShape shape;
+  
+  /**
+   * Creates a TileIcon
+   * @param shape
+   * @param width
+   * @param height
+   */
+  public TileIcon(MoveableShape shape, int width, int height) {
+    this.shape = shape;
+    this.width = width;
+    this.height = height;
+  }
+  
+  
+  /**
+   * Access method for icon's width
+   * @return width
+   */
+  public int getIconWidth() {
+    return width;
+  }
+  
+  
+  /**
+   * Access method for icon's height
+   * @return height
+   */
+  public int getIconHeight() {
+    return height;
+  }
+  
+  
+  /**
+   * Draws icon
+   */
+  public void paintIcon(Component c, Graphics g, int x, int y) {
+    Graphics2D g2 = (Graphics2D) g;
+    shape.draw(g2);
+  }
 }
