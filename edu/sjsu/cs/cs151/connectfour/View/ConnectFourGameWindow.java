637b86668c7c68318a7d6a5759aa1a5786084a8e
Anferensis <anferensis@gmail.com>
2019-03-23 16:18:38 -0700

    Add files via upload

--- edu/sjsu/cs/cs151/connectfour/View/ConnectFourGameWindow.java
+++ edu/sjsu/cs/cs151/connectfour/View/ConnectFourGameWindow.java
@@ -4,421 +4,168 @@
  * CS 151 Spring 2019
  * Professor Katarzyna Tarnowska
  * 
- * Game window for Connect Four. 
+ * Game window for Connect Four. Creates UI elements.
  * 
  * @author Krish Ghiya, Holly Lind, and Albert Ong
- * @since 20.03.2019
+ * @since 23.03.2019
  * 
  * TODO:
- *   Indicate who's turn it it. 
+ *   Add timer for Player 1/Player 2 turns. 
+ *   Indicate who's turn it is. 
  *   checkTie() 
  */
 
 package edu.sjsu.cs.cs151.connectfour.View;
 
+import edu.sjsu.cs.cs151.connectfour.Model.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Arrays;
+import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import javax.swing.*;
 
 
 public class ConnectFourGameWindow extends JFrame implements ActionListener {
+
+  private ArrayList<ArrayList<Button>> buttons;
   
-  // A two dimensional ArrayList that represents the board of Connect Four. 
-  private ArrayList<ArrayList<Tile>> board = new ArrayList<ArrayList<Tile>>();
-  
-  // A string that represents the current player. Defaults to player 1. 
-  private String current_player = "Player 1";
-  
-  //Retrieves the current working directory. 
+  // Retrieves the current working directory. 
   private String cwd = System.getProperty("user.dir");
   
+  // Creates a game object. 
+  private ConnectFourGame game;
   
-  /** Class constructor for CenterSideGameWindow.
-   * 
+  /** Constructor for the ConnectFourGameWindow. 
+   * initializes game window
+   * @postcondition window is created with blank board
    */
   public ConnectFourGameWindow() {
     
-    // Assigns the title, size, and background color of the window. 
+    // Initializes variables
+    buttons = new ArrayList<ArrayList<Button>>();
+    game = new ConnectFourGame();
+    
+    // Sets title, dimension, and background color of the window. 
     setTitle("Connect Four");
     setSize(new Dimension(1600, 900));
+    setResizable(false);
     getContentPane().setBackground(Color.WHITE);
     
-    // Retrieves and sets the window icon. 
+    // Sets the window icon. 
     ImageIcon icon = new ImageIcon(cwd + "\\images\\window_icon.png");
     setIconImage(icon.getImage());
     
-    // Retrieves and displays the background image. 
+    // Sets the background image. 
     JLabel background = new JLabel();
     background.setIcon(new ImageIcon(cwd + "\\images\\ConnectFourGameWindow_background.png"));
     setContentPane(background);
-  
-    // Adds the grid layout. 
+    
+    // Sets the button layout. 
     GridBagLayout layout = new GridBagLayout();
     setLayout(layout);
-    
     GridBagConstraints gbc = new GridBagConstraints();
     
-    // Initializing the grid of buttons. 
-    for (int y = 0; y < 6; y++) {
+    // Initialize the grid of Buttons
+    for (int x = 0; x < Board.getColumns(); x++) {
+      buttons.add(new ArrayList<Button>());
       
-      ArrayList<Tile> row = new ArrayList<Tile>();
-      
-      for (int x = 0; x < 7; x++) {
-        
+      for (int y = Board.getRows() - 1; y >= 0; y--) {
         gbc.gridx = x;
         gbc.gridy = y;
         
-        Tile add_button = new Tile(x, y);
+        Button add_button = new Button(x, y);
         
-        // Sets the size of the button. 
         add_button.setPreferredSize(new Dimension(100, 100)); 
-        
-        // Makes the button blank, both visually and internally. 
         add_button.setOpaque(false);
         add_button.setContentAreaFilled(false);
         add_button.setBorderPainted(false);
         
-        // Assigns the action listener to the button. 
         add_button.addActionListener(this);
+        buttons.get(x).add(add_button);
         
-        row.add(add_button);
         add(add_button, gbc);
       }
-      // Add each row to the board. 
-      board.add(row);
     }
     
-    setResizable(false);
+    // Makes the window visible. 
     setVisible(true);
-  } 
+  }
   
   
-  /** A method that activates whenever a button is pressed. 
-   * 
-   */
+  /** The method that activates whenever a button is pressed. */
   public void actionPerformed(ActionEvent event) {
     
-    // Retrieves the button object that was pressed. 
-    Tile button = (Tile)event.getSource();
+    // Retrieves the button that was pressed. 
+    Button button = (Button) event.getSource();
     
-    // Retrieves the x coordinate of the button as well as the 
-    // list of viable column coordinates. 
     int x_coord = button.getXCoord();
-    ArrayList<Integer> viable_columns = getViableColumns();
+    int y_coord;
+    String current_player = game.getCurrentPlayer();
     
-    // Checks if the current column is viable. 
-    boolean is_viable_column = viable_columns.contains(x_coord);
+    String result = game.oneTurn(current_player, x_coord);
     
-    // If a viable column was selected...
-    if (is_viable_column) {
+    if (result.substring(0, 12).equals("piece placed")) {
+      y_coord = Integer.parseInt(result.substring(19));
+      drawNewPiece(current_player, x_coord, y_coord);
+    }
     
-      // Retrieves the column that was selected. 
-      ArrayList<ArrayList<Tile>> columns = getColumns();
-      ArrayList<Tile> current_column = columns.get(button.getXCoord());
+    else if (result.substring(0, 3).equals("win")) {
+      y_coord = Integer.parseInt(result.substring(13));
+      drawNewPiece(current_player, x_coord, y_coord);
       
-      // Retrieves the last element, or the botton, of the column. 
-      Tile current_tile = current_column.get(5);
+      winnerDialogBox();
+    }
+  }
+  
+  
+  // draws a piece that's just been placed
+  private void drawNewPiece(String current_player, int x_coord, int y_coord) {
+    String icon_path;
+    Button button = buttons.get(x_coord).get(y_coord);
+    
+    if (current_player.equals(ConnectFourGame.getPlayerOne()))
+          icon_path = "\\images\\button_red.png";
+    else
+          icon_path = "\\images\\button_black.png";
+    
+    button.setIcon(new ImageIcon(cwd + icon_path));
+  }
+  
+  
+  /** Displays message after someone wins after closing dialog box, 
+   * prompts for new game
+   */
+  private void winnerDialogBox() {
+    
+    // Styles the message font. 
+    Font message_font = new Font("Arial", Font.BOLD, 60);
+    
+    // Displays the winner message. 
+    JLabel winner_message = new JLabel(game.getCurrentPlayer() + " wins!");
+    winner_message.setFont(message_font);
+    JOptionPane.showMessageDialog(this, winner_message);
+    
+    // Promps the player to play again. 
+    JLabel play_again_message = new JLabel("Play again?");
+    play_again_message.setFont(message_font);
+    int play_again = JOptionPane.showConfirmDialog(this, play_again_message, null, JOptionPane.YES_NO_OPTION);
+    
+    // If the yes button was pressed...
+    if (play_again == JOptionPane.YES_OPTION) {
       
-      // Iterates starting from the bottom of the column. 
-      for (int index = 5; index >= 0; index--) {
-      
-        // Checks if the current Tile is not filled. 
-        Tile test_tile = current_column.get(index);
-        boolean not_filled = !test_tile.getFilled();
-        
-        // The current tile will be the first tile that is not filled. 
-        if (not_filled) {
-          current_tile = test_tile;
-          break;
+        // Resets the game logic. 
+        game.newGame();
+        for (int x = 0; x < Board.getColumns(); x++) {
+          for (int y = 0; y < Board.getRows(); y++) {
+            Button currentButton = buttons.get(x).get(y);
+            currentButton.setIcon(new ImageIcon());
+          }
         }
-      }
-      // By the end of this loop, current_tile represents the lowest, 
-      // empty tile in the column -or in other words the tile in the column
-      // that will be filled. 
-      
-      // Variables that will be changed in the current tile. 
-      String tile_color; 
-      String icon_path;
-      String next_player;
-      
-      
-      // The new values if the current player is Player 1. 
-      if (current_player == "Player 1") {
-        tile_color = "RED";
-        icon_path = "\\images\\button_red.png";
-        next_player = "Player 2";
-      }
-      
-      // The new values if the current player is Player 2. 
-      else {
-        tile_color = "BLACK";
-        icon_path = "\\images\\button_black.png";
-        next_player = "Player 1";
-      }
-      
-      // Changes the color and filled properties to the tile. 
-      current_tile.setColor(tile_color);
-      current_tile.setIcon(new ImageIcon(cwd + icon_path));
-      current_tile.setFilled(true);
-      
-      // Changes the current player to the next player. 
-      current_player = next_player;
     }
     
-    // Checks which player won the game. 
-    String winner = checkWin();
-    
-    if (winner == "Player 1" || winner == "Player 2") {
-      
-      Font message_font = new Font("Arial", Font.BOLD, 60);
-      
-      // Displays who won the game.
-      JLabel winner_message = new JLabel(winner + " wins!");
-      winner_message.setFont(message_font);
-      JOptionPane.showMessageDialog(this, winner_message);
-      
-      // Prompts the user if they want to play again. 
-      JLabel play_again_message = new JLabel("Play again?");
-      play_again_message.setFont(message_font);
-      int play_again = JOptionPane.showConfirmDialog(this, play_again_message, null, JOptionPane.YES_NO_OPTION);
-      
-      // Restarts the game if 'yes' was selected. 
-      if (play_again == JOptionPane.YES_OPTION) {
-        restart();
-      }
-      // Closes the window if 'no' was selected. 
-      else {
-        dispose();
-      }
+    // If the no button was pressed...
+    else {
+      dispose(); // Closes the window. 
     }
   }
-  
-  
-  /* Returns all of the columns of the board. 
-   */
-  public ArrayList<ArrayList<Tile>> getColumns() {
-    
-    // A list of list of tiles that represent the columns on the board. 
-    ArrayList<ArrayList<Tile>> columns = new ArrayList<ArrayList<Tile>>();
-    
-    // Iterates though each column index in the board. 
-    for (int index = 0; index < 7; index++) {
-      
-      ArrayList<Tile> column = new ArrayList<Tile>();
-      
-      // Iterates through each row on the board and adds the element
-      // at a given column index. 
-      for (ArrayList<Tile> row : board) {
-        column.add(row.get(index));
-      }
-      
-      columns.add(column);
-    }
-    
-    return columns;
-  }
-  
-  
-  /* Returns a list of every viable column where a piece can be played. 
-   * This method determines where the next player can go next. 
-   */
-  public ArrayList<Integer> getViableColumns() {
-    
-    // A list of indices that represent the viable columns on the board. 
-    ArrayList<Integer> viable_columns = new ArrayList<Integer>();
-    
-    // Retrieves the first row in the board. 
-    ArrayList<Tile> first_row = board.get(0);
-    
-    // Iterates through the first row. 
-    for (Tile tile : first_row) {
-      
-      boolean not_filled = !tile.getFilled();
-      
-      // The column is viable if first element in the column is empty. 
-      if (not_filled) {
-        viable_columns.add(new Integer(tile.getXCoord()));
-      }
-    }
-    
-    return viable_columns;
-  }
-  
-  
-  /** Check if either Player 1 or Player 2 has won
-   * and returns the winner. 
-   */
-  public String checkWin() {
-    
-    // Assembling the row wins. 
-    ArrayList<ArrayList<Tile>> row_wins = new ArrayList<ArrayList<Tile>>();
-    
-    for (ArrayList<Tile> row : board) {
-      for (int index = 0; index < 4; index++) {
-        
-        List<Tile> sub_list = row.subList(index, index + 4);
-        ArrayList<Tile> row_win = new ArrayList(sub_list);
-        
-        row_wins.add(new ArrayList(row_win));
-      }
-    }
-    
-    // Assembling the column wins. 
-    ArrayList<ArrayList<Tile>> column_wins = new ArrayList<ArrayList<Tile>>();
-    
-    for (ArrayList<Tile> column : getColumns()) {
-      for (int index = 0; index < 3; index++) {
-        
-        List<Tile> sub_list = column.subList(index, index + 4);
-        ArrayList<Tile> column_win = new ArrayList(sub_list);
-        
-        column_wins.add(column_win);
-      }
-    }
-    
-   
-    // Assembling the diagonal wins.
-    
-    /* The starting coordinates for diagonals with a positive slop. 
-     * - - - - - - -
-     * - - - - - - -
-     * - - - - - - -
-     * X - - - - - -
-     * X - - - - - -
-     * X X X X - - -
-     */
-    int[][] pos_start_coords = {{0, 3}, {0, 4}, {0, 5}, 
-                                {1, 5}, {2, 5}, {3, 5}};
-
-    /* The starting coordinates for diagonals with a negative slop. 
-     * X X X X - - -
-     * X - - - - - -
-     * X - - - - - -
-     * - - - - - - -
-     * - - - - - - -
-     * - - - - - - -
-     */
-    int[][] neg_start_coords = {{0, 2}, {0, 1}, {0, 0}, 
-                                {1, 0}, {2, 0}, {3, 0}, };
-    
-    ArrayList<ArrayList<Tile>> diagonals = getDiagonals(pos_start_coords, true);
-    diagonals.addAll(getDiagonals(neg_start_coords, false));
-    
-    ArrayList<ArrayList<Tile>> diag_wins = new ArrayList<ArrayList<Tile>>();
-    
-    for (ArrayList<Tile> diagonal : diagonals) {
-      for (int index = 0; index < diagonal.size() - 3; index++) {
-        
-        List<Tile> diag_win = diagonal.subList(index, index + 4);
-        diag_wins.add(new ArrayList(diag_win));
-      }
-    }
-    
-    // An ArrayList of every possible win condition. 
-    ArrayList<ArrayList<Tile>> win_conditions = new ArrayList<ArrayList<Tile>>();
-    win_conditions.addAll(row_wins);
-    win_conditions.addAll(column_wins);
-    win_conditions.addAll(diag_wins);
-    
-    // There is no winner by default. 
-    String winner = null;
-    
-    // Iterates through evey possible win condition. 
-    for (ArrayList<Tile> condition : win_conditions) {
-      
-      HashSet<String> check_win = new HashSet<String>();
-      
-      for (Tile tile : condition) {
-        check_win.add(tile.getColor());
-      }
-      
-      // If all the items in the win condition have the same color...
-      if (check_win.size() == 1) {
-        
-        // Retrieves the single color from the set. 
-        String color = check_win.iterator().next();
-        
-        // Player 1 wins if all four tiles are red.
-        if (color == "RED") {
-          winner = "Player 1";
-        }
-        
-        // Player 2 wins if all four tiles are black. 
-        else if (color == "BLACK") {
-          winner = "Player 2";
-        }
-      }
-    }
-    
-    // Returns the winner.
-    // This value with either be null, 'Player 1', or 'Player 2'.
-    return winner;
-  }
-  
-  
-  /** Helper function for checkWin()
-   * 
-   * Assembles the diagonals given an array of starting coordinates
-   * and the direction of the diagonals.
-   */
-  private ArrayList<ArrayList<Tile>> getDiagonals(int[][] starting_coords, 
-                                                  boolean is_pos_slope) {
-    
-    ArrayList<ArrayList<Tile>> diagonals = new ArrayList<ArrayList<Tile>>();
-    
-    for (int[] coord : starting_coords) {
-      
-      int x = coord[0];
-      int y = coord[1];
-      
-      ArrayList<Tile> diagonal = new ArrayList<Tile>();
-      
-      while (x < 7 && 0 <= y && y < 6) {
-        
-        Tile tile = board.get(y).get(x);
-        diagonal.add(tile);
-        
-        x += 1;
-        
-        if (is_pos_slope) {
-          y -= 1;
-        } 
-        else {
-          y += 1;
-        }
-      }
-      diagonals.add(diagonal);
-    }
-    
-    return diagonals;
-  }
-  
-  
-  /** Restarts the ConnectFourGameWindow
-   * 
-   * Activated when the player opts to play again. 
-   */
-  public void restart() {
-    
-    // Resets the current player to Player 1.
-    current_player = "Player 1";
-    
-    // Iterates throught every tile on the board. 
-    for (ArrayList<Tile> row : board) {
-      for (Tile tile : row) {
-        
-        // Resets the values of the tile to a those of a blank tile. 
-        tile.setIcon(new ImageIcon());
-        tile.setColor("NONE");
-        tile.setFilled(false);
-      }
-    }
-  }
-  
   
 }
--- edu/sjsu/cs/cs151/connectfour/View/ConnectFourMenuWindow.java
+++ edu/sjsu/cs/cs151/connectfour/View/ConnectFourMenuWindow.java
@@ -1,5 +1,4 @@
 
-
 /** ConnectFourMenuWindow.java
  * 
  * CS 151 Spring 2019
@@ -8,10 +7,10 @@
  * Menu window for Connect Four. 
  * 
  * @author Krish Ghiya, Holly Lind, and Albert Ong
- * @since 20.03.2019
+ * @since 23.03.2019
  * 
  * TODO:
- *   Finish implementation and functionality. 
+ *   Implement functionality for play, online play, and about buttons. 
  */
 
 package edu.sjsu.cs.cs151.connectfour.View;
@@ -24,7 +23,7 @@ import javax.swing.*;
 
 public class ConnectFourMenuWindow extends JFrame implements ActionListener {
 
-  //Retrieves the current working directory. 
+  // Retrieves the current working directory. 
   // This is used primarily for accessing image files. 
   private String cwd = System.getProperty("user.dir");
   
@@ -44,29 +43,42 @@ public class ConnectFourMenuWindow extends JFrame implements ActionListener {
     
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.gridx = 0;
-    gbc.insets = new Insets(0, 0, 35, 0);
+    gbc.insets = new Insets(0, 0, 20, 0);
     
     JLabel logo = new JLabel(new ImageIcon(cwd + "\\images\\ConnectFour_logo.png"));
     logo.setAlignmentX(Component.CENTER_ALIGNMENT);
     add(logo, gbc);
 
     
-    String[] button_names = {"Play", "About", "Exit"};
+    String[] button_names = {"PLAY", "ONLINE_PLAY", "ABOUT", "EXIT"};
     
     for (String name : button_names) {
       
+      // Creates a new button object for each button name. 
       JButton button = new JButton();
+      
+      // Assigns the button name, size, and action listener. 
       button.setName(name);
-      button.setAlignmentX(Component.CENTER_ALIGNMENT);
       button.setPreferredSize(new Dimension(400, 80)); 
       button.addActionListener(this);
       
+      // Aligns the button to the center of the window. 
+      button.setAlignmentX(Component.CENTER_ALIGNMENT);
+      
+      // Makes the border and content of the button opqaue. 
       button.setOpaque(false);
       button.setContentAreaFilled(false);
       button.setBorderPainted(false);
       
-      String icon_path = cwd + "\\images\\menu_button_" + name.toLowerCase() + ".png";
-      button.setIcon(new ImageIcon(icon_path));
+      // Finds the path to the image icons. 
+      String image_path = cwd + "\\images\\menu_button_" + name.toLowerCase();
+      String button_select_image = image_path + "_select.png";
+      String button_deselect_image = image_path + "_deselect.png";
+      
+      // Assigns the default button icon as well as the icon
+      // that appears when moused over. 
+      button.setIcon(new ImageIcon(button_deselect_image));
+      button.setRolloverIcon(new ImageIcon(button_select_image));
       
       add(button, gbc);
     }
@@ -83,14 +95,17 @@ public class ConnectFourMenuWindow extends JFrame implements ActionListener {
     
     String button_name = button.getName();
     
-    if (button_name == "Play") {
+    if (button_name == "PLAY") {
       System.out.println("Play button was pressed!");
       // changePanel(new ConnectFourGameWindow());
     }
-    else if (button_name == "About") {
-      System.out.println("About button was pressed!");
+    else if (button_name == "ABOUT") {
+      new ConnectFourAboutWindow();
     }
-    else if (button_name == "Exit") {
+    else if (button_name == "ONLINE_PLAY") {
+      System.out.println("Online play button was pressed!");
+    }
+    else if (button_name == "EXIT") {
       dispose();
     }
   }
