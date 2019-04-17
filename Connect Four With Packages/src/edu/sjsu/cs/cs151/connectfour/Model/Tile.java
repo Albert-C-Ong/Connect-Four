e973e0c7073fd9c43e770e1bb602c081c6c3e0d2
hollylind <47756054+hollylind@users.noreply.github.com>
2019-04-17 14:45:50 -0700

    Fixed model
    -renamed ConnectFourGame to Model
    -changed Board to aggregate ArrayList rather than extend ArrayList
    -changed ConnectFourGameTest to reflect model changes (so it can still be tested without a view)

---
+++ Connect Four With Packages/src/edu/sjsu/cs/cs151/connectfour/Model/Tile.java
@@ -0,0 +1,103 @@
+/** Tile.java
+ * 
+ * CS 151 Spring 2019
+ * Professor Katarzyna Tarnowska
+ * 
+ * A class the represents an individual tile on the
+ * Connect Four board.
+ * 
+ * @author Holly Lind and Albert Ong
+ * @since 23.03.2019
+ */
+
+package edu.sjsu.cs.cs151.connectfour.Model;
+
+
+public class Tile {
+  
+  
+  /**
+   * creates a Tile at coordinates (x, y)
+   * @param x_coord: x coordinate of the Tile
+   * @param y_coord: y coordinate of the Tile
+   * @postcondition Tile is colorless and unfilled
+   */
+  public Tile(int x_coord, int y_coord) {
+    this.x_coord = x_coord;
+    this.y_coord = y_coord;
+    color = "";
+    filled = false;
+  }
+  
+  
+  /**
+   * access method for x
+   * @return x_coord
+   */
+  public int getXCoord() {
+    return x_coord;
+  }
+  
+  
+  /**
+   * access method for y
+   * @return y_coord
+   */
+  public int getYCoord() { 
+    return y_coord;
+  }
+  
+  
+  /**
+   * access method for color
+   * @return color
+   */
+  public String getColor() {
+    return color;
+  }
+  
+  
+  /**
+   * access method for fill
+   * @return filled
+   */
+  public boolean getFilled() {
+    return filled;
+  }
+  
+  
+  /**
+   * modifier method for x
+   * @param newColor: String to set color to
+   */
+  public void setColor(String newColor) {
+    color = newColor;
+  }
+  
+  
+  /**
+   * sets filled to true 
+   * use after a player selects this particular Tile
+   * @postcondition Tile is filled
+   */
+  public void nowFilled() {
+    filled = true;
+  }
+  
+  
+  /**
+   * resets all of Tile parameters except for coordinates
+   * use when new game is started
+   * @postcondition Tile is colorless and unfilled
+   */
+  public void resetTile() {
+    color = "";
+    filled = false;
+  }
+  
+  
+  private int x_coord;
+  private int y_coord;
+  private String color;
+  private boolean filled;
+}
---
+++ Connect Four With Packages/test/test/ConnectFourGameTest.java
@@ -0,0 +1,95 @@
+package test;
+
+/** ConnectFourGameTest.java
+ * 
+ * CS 151 Spring 2019
+ * Professor Katarzyna Tarnowska
+ * 
+ * A class that lets you play Connect Four in the console. 
+ * 
+ * @author Holly Lind
+ * @since 3/14/2019
+ */
+import java.util.Scanner;
+
+import edu.sjsu.cs.cs151.connectfour.Model.*;
+
+
+public class ConnectFourGameTest {
+
+	// runs a Scanner to take user input
+	public static void readInput(Model game)
+	{
+		Scanner in = new Scanner(System.in);
+		String input = in.next();
+		
+		if (input.equals("p1"))
+		{
+			System.out.println(game.oneTurn(Model.getPlayerOne(), in.nextInt()));
+			printBoard(game.getBoard());
+		}
+		else if (input.equals("p2"))
+		{
+			System.out.println(game.oneTurn(Model.getPlayerTwo(), in.nextInt()));
+			printBoard(game.getBoard());
+		}
+		else if (input.equals("newgame") || input.equals("reset"))
+		{
+			game.newGame();
+			startGame();
+			printBoard(game.getBoard());
+		}
+		else
+		{
+			System.out.println("must start line with p1, p2, or newgame");
+		}
+		
+		readInput(game);
+	}
+	
+	
+	// prints the current Connect Four Board
+	public static void printBoard(Board board)
+	{
+		System.out.println("");
+		for (int row = Board.getRows() - 1; row >= 0; row--)
+		{
+			for (int column = 0; column < Board.getColumns(); column++)
+			{
+				if (!board.getTile(column, row).getFilled())
+					System.out.print("* ");
+				else if (board.getTile(column, row).getColor().equals(Board.getPlayerOneColor()))
+					System.out.print("x ");
+				else if (board.getTile(column, row).getColor().equals(Board.getPlayerTwoColor()))
+					System.out.print("o ");
+				else
+					System.out.print("? ");
+			}
+			System.out.println("");
+		}
+		System.out.println("0 1 2 3 4 5 6\n");
+	}
+	
+	
+	// prints instructions for playing the game in the console
+	public static void startGame()
+	{
+		System.out.println("x is player 1; o is player 2");
+		System.out.println("* is empty\n");
+		System.out.println("type p1 or p2 to select player, then type number for column");
+		System.out.println("ex. type \"p1 3\" to put p1's piece in column 3\n");
+		
+		System.out.println("initial board:");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Model game = new Model();
+		startGame();
+		printBoard(game.getBoard());
+		
+		readInput(game);
+	}
+	
+}
