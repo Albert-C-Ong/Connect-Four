05510b132ae02ee8fc4a500454c8de19e7a5d680
krish <krish.ghiya@sjsu.edu>
2019-04-20 18:58:43 -0700

    fixed setServerIP bug

--- src/edu/sjsu/cs/cs151/connectfour/network/Server.java
+++ src/edu/sjsu/cs/cs151/connectfour/network/Server.java
@@ -4,62 +4,72 @@ import java.io.*;
 import java.net.*;
 import edu.sjsu.cs.cs151.connectfour.View.ConnectFourMainWindow;
 
-public class Server extends Network  {
-	
+public class Server extends Network {
+
 	private ServerSocket server;
 	private DatagramSocket socket;
-	
-	//constructor
+
+	// constructor
 	public Server(ConnectFourMainWindow parent) {
 		super(parent, "Player 1");
 	}
-	
-	public void startRunning(){
+
+	public void startRunning() {
 		broadcastMessage();
-		try{
-			server = new ServerSocket(6789, 2); //6789 is a dummy port for testing, this can be changed. The 100 is the maximum people waiting to connect.
-			while(true){
-					//Trying to connect and have conversation
-					waitForConnection();
-					super.startRunning();
+		try {
+			server = new ServerSocket(8888, 2); // 8888 is a dummy port for testing, this can be changed. The 100 is
+													// the maximum people waiting to connect.
+			while (true) {
+				// Trying to connect and have conversation
+				waitForConnection();
+				super.startRunning();
 			}
-		} catch (IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
 	}
-	//wait for connection, then display connection information
-	private void waitForConnection() throws IOException{
+
+	// wait for connection, then display connection information
+	private void waitForConnection() throws IOException {
 		connection = server.accept();
 	}
-	
+
 	private void broadcastMessage() {
-	    try {
-	      //Keep a socket open to listen to all the UDP trafic that is destined for this port
-	      socket = new DatagramSocket(6789, InetAddress.getByName("0.0.0.0"));
-	      socket.setBroadcast(true);
+		try {
+			// Keep a socket open to listen to all the UDP trafic that is destined for this
+			// port
+			socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
+			socket.setBroadcast(true);
 
-	      while (true) {
-	       
-	    	//Receive a packet
-	        byte[] recvBuf = new byte[15000];
-	        DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-	        
-	        //Receive Packet
-	        socket.receive(packet);
+			while (true) {
+				System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
 
-	        //See if the packet holds the right command (message)
-	        String message = new String(packet.getData()).trim();
-	        if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
-	          byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
+				// Receive a packet
+				byte[] recvBuf = new byte[15000];
+				DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+				socket.receive(packet);
 
-	          //Send a response
-	          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
-	          socket.send(sendPacket);
-	          return;
-	        }
-	      }
-	    } catch (IOException ex) {
-	    	ex.printStackTrace();
-	    }
-	  }
+				// Packet received
+				System.out.println(getClass().getName() + ">>>Discovery packet received from: "
+						+ packet.getAddress().getHostAddress());
+				System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
+
+				// See if the packet holds the right command (message)
+				String message = new String(packet.getData()).trim();
+				
+				if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
+					byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
+
+					// Send a response
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(),
+							packet.getPort());
+					socket.send(sendPacket);
+
+					return;
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
